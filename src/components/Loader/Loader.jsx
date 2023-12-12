6476863c3ac1d1b37ad913dd6e18a38f2a6acf6c
Artem <krylov.artem.1995@gmail.com>
2023-12-12 20:48:29 +0100

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import { Oval } from 'react-loader-spinner';
-import { DivLoader } from './Loader.styled';
 
 export class Loader extends Component {
   render() {
     return (
-      <DivLoader>
-        <Oval
-          height={80}
-          width={80}
-          color="#962fb5"
-          wrapperStyle={{}}
-          wrapperClass=""
-          visible={true}
-          ariaLabel="oval-loading"
-          secondaryColor="#f20515"
-          strokeWidth={2}
-          strokeWidthSecondary={2}
-        />
-      </DivLoader>
+      <Oval
+        height={80}
+        width={80}
+        color="#962fb5"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#f20515"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+      />
     );
   }
 }
--- src/components/Loader/Loader.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const DivLoader = styled.div`
-display: 'grid',
-textAlign: 'center', 
-marginTop: '20px'
-`;
